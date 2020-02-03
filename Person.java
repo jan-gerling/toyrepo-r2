95f7a085bb9bece541a3268010ff173affdc3dd5
rafaeldurelli <rafael.durelli@ufla.br>
2020-02-03 15:27:48 -0300

    add address field

--- Person.java
+++ Person.java
@@ -1,4 +1,29 @@
 public class People {
     private String name;
     private String lastName;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
